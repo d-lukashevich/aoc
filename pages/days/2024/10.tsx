0096dc95b25ab9d8a57cafd5d9fed72089f4419f
Denis Lukashevich <lukashevich.work@gmail.com>
2024-12-10 10:36:49 +0100

    feat(10.12.2024): simplify solution

--- pages/days/2024/10.tsx
+++ pages/days/2024/10.tsx
@@ -8,50 +8,32 @@ const getData = (raw: string) => {
 };
 
 const getTops = (data: number[][]) => {
-  const heads: Pos[] = [];
-  data.forEach((row, y) => {
-    row.forEach((cell, x) => {
-      if (cell === 0) heads.push([x, y]);
+  const countTops = ([x, y]: Pos): string[] => {
+    const value = data[y]?.[x];
+    if (value === 9) return [`${x},${y}`];
+    const directions: Pos[] = [
+      [x, y - 1],
+      [x, y + 1],
+      [x - 1, y],
+      [x + 1, y],
+    ];
+    return directions.flatMap((pos) => {
+      const nValue = data[pos[1]]?.[pos[0]];
+      return nValue === value + 1 ? countTops(pos) : [];
     });
-  });
-
-  const countTops = (pos: Pos) => {
-    let tops = new Array<string>();
-    const stack = [pos];
-    while (stack.length > 0) {
-      const [x, y] = stack.pop()!;
-      const current = data[y][x];
-      if (current === 9) {
-        tops.push(`${x},${y}`);
-        continue;
-      }
-      const directions: Pos[] = [
-        [x, y - 1],
-        [x, y + 1],
-        [x - 1, y],
-        [x + 1, y],
-      ];
-      directions.forEach(([nx, ny]) => {
-        const value = data[ny]?.[nx] ?? 0;
-        if (value === current + 1) {
-          stack.push([nx, ny]);
-        }
-      });
-    }
-    return tops;
   };
 
-  return heads.map(countTops);
+  return data.flatMap((row, y) => {
+    return row.map((cell, x) => (cell === 0 ? countTops([x, y]) : []));
+  });
 };
 
 const first = (raw: string) => {
-  const data = getData(raw);
-  return getTops(data).reduce((acc, tops) => acc + new Set(tops).size, 0);
+  return getTops(getData(raw)).reduce((acc, tops) => acc + new Set(tops).size, 0);
 };
 
 const second = (raw: string) => {
-  const data = getData(raw);
-  return getTops(data).reduce((acc, tops) => acc + tops.length, 0);
+  return getTops(getData(raw)).reduce((acc, tops) => acc + tops.length, 0);
 };
 
 export default function Day() {
