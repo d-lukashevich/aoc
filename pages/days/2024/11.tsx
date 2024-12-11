305d79036bba684d2669c99fbb109be8e1edbb47
Denis Lukashevich <lukashevich.work@gmail.com>
2024-12-11 11:56:05 +0100

    refactor(11.12.2024): clean up

--- pages/days/2024/11.tsx
+++ pages/days/2024/11.tsx
@@ -4,25 +4,17 @@ import { withCache } from '@utils';
 const blink = (num: number): number[] => {
   if (num === 0) return [1];
   const str = num.toString();
-  if (str.length % 2 === 0) {
-    return [str.slice(0, str.length / 2), str.slice(str.length / 2)].map(Number);
-  }
+  if (str.length % 2 === 0) return [str.slice(0, str.length / 2), str.slice(str.length / 2)].map(Number);
   return [num * 2024];
 };
 
 const doBlinks = withCache((arr: number[], depth: number): number => {
   if (!depth) return arr.length;
-  return arr.reduce((acc, num) => {
-    const sub = blink(num);
-    return acc + doBlinks(sub, depth - 1);
-  }, 0);
+  return arr.reduce((acc, num) => acc + doBlinks(blink(num), depth - 1), 0);
 });
 
 const makeSolution = (times: number) => {
-  return (raw: string) => {
-    const data = raw.split(' ').map(Number);
-    return doBlinks(data, times);
-  };
+  return (raw: string) => doBlinks(raw.split(' ').map(Number), times);
 };
 
 const first = makeSolution(25);
