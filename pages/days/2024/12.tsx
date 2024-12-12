0b421c69eb01cf7eb66f74ae8eb4f4b2fa797c5f
Denis Lukashevich <lukashevich.work@gmail.com>
2024-12-12 09:51:00 +0100

    feat(12.12.2024): clean up

--- pages/days/2024/12.tsx
+++ pages/days/2024/12.tsx
@@ -2,13 +2,13 @@ import { PuzzleRenderer } from '@units/PuzzleRenderer';
 import { parseRows } from '@utils';
 
 type Fence = [boolean, boolean, boolean, boolean];
-type Region = [number, number, number];
+type Region = { area: number; perimeter: number; sides: number };
 
 const getData = (raw: string) => {
   const data = parseRows(raw).map((row) => row.split(''));
   const seen = new Set<string>();
 
-  const giveNeighbors = (x: number, y: number) => [
+  const getNeighbors = (x: number, y: number) => [
     [x, y - 1],
     [x + 1, y],
     [x, y + 1],
@@ -18,39 +18,33 @@ const getData = (raw: string) => {
   const getFences = (x: number, y: number, letter: unknown): Fence => {
     const diffLetter = data[y]?.[x];
     if (diffLetter !== letter) return [false, false, false, false];
-    return giveNeighbors(x, y).map(([nx, ny]) => data[ny]?.[nx] !== diffLetter) as Fence;
+    return getNeighbors(x, y).map(([nx, ny]) => data[ny]?.[nx] !== diffLetter) as Fence;
   };
 
-  const calcRegion = (x: number, y: number): [number, number, number] => {
+  const calcSides = (x: number, y: number) => {
+    const fences = getFences(x, y, data[y][x]);
+    const [, rightFence, , leftFence] = getFences(x, y - 1, data[y][x]);
+    const [topFence, , bottomFence] = getFences(x - 1, y, data[y][x]);
+    const comparisonFences = [topFence, rightFence, bottomFence, leftFence];
+    return fences.reduce((acc, fence, i) => acc + +(fence && !comparisonFences[i]), 0);
+  };
+
+  const calcRegion = (x: number, y: number): Region => {
     const letter = data[y][x];
-    let perimeter = 0;
-    let area = 1;
-    let sides = 0;
     seen.add(`${x},${y}`);
 
-    const fences = getFences(x, y, letter);
-    const [, rightFence, , leftFence] = getFences(x, y - 1, letter);
-    const [topFence, , bottomFence] = getFences(x - 1, y, letter);
-    const comparisonFences = [topFence, rightFence, bottomFence, leftFence];
-    fences.forEach((fence, i) => {
-      if (fence && !comparisonFences[i]) sides++;
-    });
-
-    giveNeighbors(x, y).forEach(([nx, ny]) => {
+    const region = { area: 1, perimeter: 0, sides: calcSides(x, y) };
+    return getNeighbors(x, y).reduce((acc, [nx, ny]) => {
       const val = data[ny]?.[nx];
-      if (val !== letter) {
-        perimeter++;
-        return;
+      if (val !== letter) acc.perimeter++;
+      if (val === letter && !seen.has(`${nx},${ny}`)) {
+        const { area, perimeter, sides } = calcRegion(nx, ny);
+        acc.perimeter += perimeter;
+        acc.area += area;
+        acc.sides += sides;
       }
-      if (data[ny][nx] === letter && !seen.has(`${nx},${ny}`)) {
-        const [a, p, s] = calcRegion(nx, ny);
-        perimeter += p;
-        area += a;
-        sides += s;
-      }
-    });
-
-    return [area, perimeter, sides];
+      return acc;
+    }, region);
   };
 
   const regions: Region[] = [];
@@ -62,10 +56,10 @@ const getData = (raw: string) => {
 };
 
 const first = (raw: string) => {
-  return getData(raw).reduce((acc, [area, perimeter]) => acc + perimeter * area, 0);
+  return getData(raw).reduce((acc, { area, perimeter }) => acc + perimeter * area, 0);
 };
 const second = (raw: string) => {
-  return getData(raw).reduce((acc, [area, , sides]) => acc + sides * area, 0);
+  return getData(raw).reduce((acc, { area, sides }) => acc + sides * area, 0);
 };
 
 export default function Day() {
